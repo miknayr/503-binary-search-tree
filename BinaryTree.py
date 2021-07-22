c9417b7a6b523c4d9eea650e736b16093380a9b8
miknayr <teamry4n@gmail.com>
2021-07-22 12:37:04 -0700

    search and print solution with node class

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,20 +1,44 @@
 class Node:
-  # here we will add the constructor
-  # and the string method
-  pass
-
-
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+  # method override of string method for the node
+  def __str__ (self):
+    return f'{self.data}'
 class BinaryTree:
-  # here we will add the constructor
-
+  def __init__(self):
+    self.root = None
   def insert(self, data):
     '''
       Insert(data: any) -> None:\n 
       creates a new Node from the data passed in and adds it to the tree
       If the data is already in the tree, does not insert it again
     '''
-    pass
-
+    #first make a node from the data
+    new_node = Node(data)
+    # if there is no root, set node to be root
+    if not self.root:
+      self.root = new_node
+      return
+    # loop over the tree starting at root and do comparisons
+    current_node = self.root
+    while current_node:
+      # if the data is smaller than the current node
+      if new_node.data < current_node.data:
+        # if there is no left
+        if not current_node.left:
+          # set the left to be the new node
+          current_node.left = new_node
+          return
+        else: current_node = current_node.left
+      # if the new node's data is bigger than the current node
+      elif new_node.data > current_node.data:
+        if not current_node.right:
+          current_node.right = new_node
+          return
+        else: current_node = current_node.right
+      else: return # return if duplicate
   def search(self, val):
     '''
       search(value: any) -> value or bool:\n 
@@ -23,16 +47,42 @@ class BinaryTree:
       If the node exists, return it
       If the node doesn't exist, return false
     '''
-    pass
-
+    # check if the tree is empty
+    if not self.root:
+      return False
+    # loop through the tree starting at the root
+    current_node = self.root
+    while current_node:
+      # if the value is less than the current node
+      if val < current_node.data:
+        # set current node to be the left
+        current_node = current_node.left
+      # else if the value is greater
+      elif val > current_node.data:
+        # set current node to be the right
+        current_node = current_node.right
+      # else return the node
+      else:
+        return current_node
+    return False
   def print(self, node=None):
     '''
       print(node=optional: Node) -> None:\n
       prints out all values recursively (in a breadth first search fashion)
       defualt start is at root node
     '''
-    pass
-
+    # check if this is the first recursion
+    if not node: node = self.root
+    # print the node
+    print(node)
+    # if there is a left node
+    if node.left:
+      # recursively invoke self.print on the node
+      self.print(node.left)
+    # if there is a right node
+    if node.right:
+      # recurse...
+      self.print(node.right)
   def size(self, node=None):
     '''
       size(node=optional: Node) -> int:\n 
@@ -41,7 +91,6 @@ class BinaryTree:
       If no node is provided, we can use the root as a sensible default
     '''
     pass
-
   def height(self, node=None):
     '''
       height(node=optional: Node) -> int:\n 
@@ -50,7 +99,6 @@ class BinaryTree:
       If not given a specific node, default to using the root node
     '''
     pass
-
   def get_max(self):
     '''
       getMax() -> int:\n 
@@ -58,7 +106,6 @@ class BinaryTree:
       Calculate the maximum value held in the tree
     '''
     pass
-
   def get_min(self):
     '''
       getMin() -> int:\n 
---
+++ main.py
@@ -0,0 +1,18 @@
+from BinaryTree import Node
+from BinaryTree import BinaryTree
+# tester node
+my_node = Node('hello')
+# print(my_node)
+my_BST = BinaryTree()
+my_BST.insert(10)
+# print(my_BST.root)
+my_BST.insert(12)
+my_BST.insert(8)
+# print(my_BST.root.right)
+# my_BST.print()
+my_BST.insert(15)
+my_BST.insert(9)
+my_BST.insert(3)
+my_BST.insert(21)
+my_BST.insert(87)
+print(my_BST.search(87))
